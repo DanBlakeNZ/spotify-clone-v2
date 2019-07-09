bc1295d0e76d48fdd27d6c776c239ee3289bba71
DanBlakeNZ <dblakenz@hotmail.com>
2019-07-09 17:13:06 +1000

    Set up for NavBar

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 let isAuthenticated = true; // TODO: check is logged in here.
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} component={props => (isAuthenticated ? <Component {...props} /> : <Redirect to="/" />)} />
+  <Route
+    {...rest}
+    component={props =>
+      isAuthenticated ? (
+        <div>
+          <NavBar /> <Component {...props} />{" "}
+        </div>
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
 );
 
 export default PrivateRoute;
